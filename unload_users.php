93c73a09de9590bc7a0c0ab7259cdfeb4f5c88ba
sad-coder <gebsss29@gmail.com>
2020-07-01 14:37:22 +0600

    unload users

--- unload_users.php
+++ unload_users.php
@@ -1,56 +1,40 @@
 <?php
 
+use Helpers\ConfigHelper;
 use Helpers\PhoneHelper;
 
 require __DIR__ . '/vendor/autoload.php';
 
-
-
-//$testController = new \Controllers\UserController();
-//
-//$testController->checkExist('77077111154');
-
 $capsule = new \Illuminate\Database\Capsule\Manager();
+$config = ConfigHelper::getDbConfig('mobile_db');
+
 $capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'indigo24Mobile',
-    'username'  => 'admin',
-    'password'  => '123',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
+    'driver' => $config['driver'],
+    'host' => $config['host'],
+    'database' => $config['database'],
+    'username' => $config['username'],
+    'password' => $config['password'],
+    'charset' => $config['charset'],
+    'collation' => $config['collation'],
+    'prefix' => $config['prefix'],
 ]);
 
 $capsule->setAsGlobal();
-
-
-
 $capsule = new \Illuminate\Database\Capsule\Manager();
 
-$users = \Illuminate\Database\Capsule\Manager::table('customers')->get();
-$redis = new \Redis();
-$redis->connect('127.0.0.1',6379);
+while (true){
+    $users = \Illuminate\Database\Capsule\Manager::table('customers')->get();
+    $redis = new \Redis();
+    $redis->connect('127.0.0.1',6379);
+    foreach ($users as $user) {
+        $phoneInCorrectFormat = PhoneHelper::replaceForSeven($user->phone);
+        $redis->zAdd("users:phones", ['NX'],$phoneInCorrectFormat,$user->id);
 
-
-//$test = $redis->zRevRangeByScore('user:chats:1','+inf','-inf',['limit'=>[20,40]]);
-//
-//dd($test);
-
-
-foreach ($users as $user) {
-    $phoneInCorrectFormat = PhoneHelper::replaceForSeven($user->phone);
-//    $redis->zAdd("users:phones", ['NX'],$phoneInCorrectFormat,$user->id);
-//    $redis->zAdd("users:phones", ['NX'],$phoneInCorrectFormat,$user->id);
-//    $redis->hSet($user->id,'token',$user->unic);
-//    $redis->zAdd("users:avatars", ['CX'],$user->id,$user->avatar);
-
-//    $redis->set("user:avatar:{$user->id}",$user->avatar);
-//    $redis->set("user:name:{$user->id}",$user->name);
-    $redis->set("user:email:{$user->id}",$user->email);
-
-
-//    $redis->zAdd("users:names",['CX'],$user->id,$user->name);
+        $redis->set("user:avatar:{$user->id}",$user->avatar);
+        $redis->set("user:name:{$user->id}",$user->name);
+        $redis->set("user:email:{$user->id}",$user->email);
+    }
+    $redis->close();
 }
 
 
