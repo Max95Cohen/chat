88d8a41b994081a3c2f92a40ccaa910a6e81bb2c
Aibek Q <aq@indigo24.com>
2020-10-07 18:17:04 +0600

    add db.conf.example.

---
+++ config/db.conf.example.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'chat_db' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'chat',
+        'username' => 'mobile',
+        'password' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+    ],
+    'mobile_db' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'indigo24Mobile',
+        'username' => 'mobile',
+        'password' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+    ]
+];
